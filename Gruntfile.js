dff3c501f2d38ba7811dbf9cc986a193abafb0bf
Andrew Haigh <nelfin@gmail.com>
2015-01-18 00:04:33 +1100

    Add grunt-contrib-concat to squish files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,24 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['lib/**/*.js'],
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
           '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'lib/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: ['<%= concat.dist.dest %>'],
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js'
       }
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify']);
 }
